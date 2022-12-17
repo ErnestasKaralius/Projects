5d59b68be65e1be04f98bc97c0d1f5b15547277d
Ernest <78487146+ErnestasKaralius@users.noreply.github.com>
2022-12-17 10:40:01 +0200

    Add files via upload

--- Roblox/Town Destroyers/ServerScriptService/admin_commands.lua
+++ Roblox/Town Destroyers/ServerScriptService/admin_commands.lua
@@ -1,14 +1,16 @@
---!strict
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ServerStorage = game:GetService("ServerStorage")
-local Players = game:GetService("Players")
 
-local display = game.ReplicatedStorage.DisplayMessage
-local gears = ServerStorage:GetChildren()
+local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local gear_essentials = require(ReplicatedStorage.Modules.GearEssentials)
+
+local display = ReplicatedStorage.DisplayMessage
+local gears = ServerStorage.Gears:GetChildren()
 
 local commands : {[string]: any}
 commands = {
-	["/cmds"] = function(player)
+	["/cmds"] = function(player : Player)
 		local text = ""
 		for key : string, value in pairs(commands) do
 			if key ~= "/cmds" then
@@ -17,31 +19,30 @@ commands = {
 		end
 		display:FireClient(player, text, Color3.new(0.8, 0.3, 0.4), 0, 0, true)
 	end,
-	["/gear "] = function(player, tool)
-		local character = workspace:FindFirstChild(player.Name)
-		if character then
-			local humanoid = character:FindFirstChild("Humanoid")
-			if humanoid and humanoid.Health ~= 0 then
-				local new_gear
-				
-				if tool:lower() == "all" then
-					for _, gear in pairs(gears) do
-						new_gear = gear:Clone()
-						new_gear.Parent = player.Backpack
-					end
-				else
-					for _, gear in pairs(gears) do
-						if gear.Name:lower() == tool:lower() then
-							new_gear = gear:Clone()
-							new_gear.Parent = player.Backpack	
-							return
-						end
-					end
-				end
+	["/gear "] = function(player : Player, tool : string)
+		local character, humanoid = table.unpack(gear_essentials.get_character_from_player(player))
+		if not gear_essentials.character_alive(humanoid) then return end
+		
+		local backpack = player:FindFirstChild("Backpack")
+		local new_gear
+			
+		if tool:lower() == "all" then
+			for _, gear in pairs(gears) do
+				new_gear = gear:Clone()
+				new_gear.Parent = backpack
+			end
+			return
+		end
+		
+		for _, gear in pairs(gears) do
+			if gear.Name:lower() == tool:lower() then
+				new_gear = gear:Clone()
+				new_gear.Parent = backpack
+				return
 			end
 		end
 	end,
-	["/money "] = function(player, money)
+	["/money "] = function(player : Player, money : number)
 		if tonumber(money) then
 			local leaderstats = player:FindFirstChild("leaderstats") :: Folder
 			if not leaderstats then return end
@@ -53,45 +54,43 @@ commands = {
 		end
 	end,
 	["/speed "] = function(player, value)
-		local character = workspace:FindFirstChild(player.Name)
-		if character then
-			local humanoid = character:FindFirstChild("Humanoid")
-			if humanoid and humanoid.Health ~= 0 then
-				if tonumber(value) then
-					humanoid.WalkSpeed = value
-				elseif 
-					value:lower() == "normal" or value:lower() == "default" then
-					humanoid.WalkSpeed = 16
-				end
-			end
+		local character, humanoid = table.unpack(gear_essentials.get_character_from_player(player))
+		if not gear_essentials.character_alive(humanoid) then return end
+		
+		if tonumber(value) then
+			humanoid.WalkSpeed = value
+		elseif value:lower() == "normal" or value:lower() == "default" then
+			humanoid.WalkSpeed = 16
 		end
 	end,
-	["/kick "] = function(player, person)
+	["/kick "] = function(player : Player, person : string)
 		local players = Players:GetChildren()
 		
 		if person:lower() == "all" then
 			for _, player in pairs(players) do
 				player:Kick()
 			end	
-		else
-			if person:lower() == "me" then
-				person = player.Name
-			end
-			for _, player in pairs(players) do
-				if player.Name:lower() == person:lower() then
-					player:Kick()
-					return
-				end
+			return
+		end
+		
+		if person:lower() == "me" then
+			person = player.Name
+		end
+		
+		for _, player in pairs(players) do
+			if player.Name:lower() == person:lower() then
+				player:Kick()
+				return
 			end
 		end
 	end, 
-	["/kill "] = function(player, person)
+	["/kill "] = function(player : Player, person : string)
 		local models = workspace:GetChildren()
 		
 		if person:lower() == "all" then
 			for _, character in pairs(models) do
 				local humanoid = character:FindFirstChild("Humanoid")
-				if humanoid and humanoid.Health ~= 0 then
+				if gear_essentials.character_alive(humanoid) then
 					humanoid.Health = 0
 				end
 			end	
@@ -102,7 +101,7 @@ commands = {
 			for _, character in pairs(models) do
 				if character.Name:lower() == person:lower() then
 					local humanoid = character:FindFirstChild("Humanoid")
-					if humanoid and humanoid.Health ~= 0 then
+					if gear_essentials.character_alive(humanoid) then
 						humanoid.Health = 0
 					end
 					return
@@ -111,29 +110,21 @@ commands = {
 		end
 	end,
 	["/god"] = function(player)
-		local character = workspace:FindFirstChild(player.Name)
-		if character then
-			local humanoid = character:FindFirstChild("Humanoid")
-			if humanoid and humanoid.Health ~= 0 then
-				if not character:FindFirstChild("GodForceField") then
-					local forcefield = Instance.new("ForceField")
-					forcefield.Name = "GodForceField"
-					forcefield.Visible = false
-					forcefield.Parent = character
-				end
-			end
-		end
+		local character, humanoid = table.unpack(gear_essentials.get_character_from_player(player))
+		if not gear_essentials.character_alive(humanoid) or character:FindFirstChild("GodForceField") then return end
+		
+		local forcefield = Instance.new("ForceField")
+		forcefield.Name = "GodForceField"
+		forcefield.Visible = false
+		forcefield.Parent = character
 	end,
 	["/ungod"] = function(player)
-		local character = workspace:FindFirstChild(player.Name)
-		if character then
-			local humanoid = character:FindFirstChild("Humanoid")
-			if humanoid and humanoid.Health ~= 0 then
-				local forcefield = character:FindFirstChild("GodForceField")
-				if forcefield then
-					forcefield:Destroy()
-				end
-			end
+		local character, humanoid = table.unpack(gear_essentials.get_character_from_player(player))
+		if not gear_essentials.character_alive(humanoid) then return end
+		
+		local forcefield = character:FindFirstChild("GodForceField")
+		if forcefield then
+			forcefield:Destroy()
 		end
 	end,
 	["/tp "] = function(player, arguments)
@@ -154,28 +145,21 @@ commands = {
 			elseif character.Name:lower() == people[2]:lower() then
 				player2 = character
 			end
-		end	
+		end
+		
+		if not gear_essentials.character_alive(player2) then return end
 		
 		if people[1]:lower() == "all" then
-			if player2 and player2:FindFirstChild("Humanoid") then
-				if player2.Humanoid.Health ~= 0 then
-					for _, character in pairs(models) do
-						if character and character:FindFirstChild("Humanoid") then
-							if character.Humanoid.Health ~= 0 then
-								character:MoveTo(player2.Head.Position)
-							end	
-						end
-					end
+			for _, character in pairs(models) do
+				if gear_essentials.character_alive(character) then
+					character:MoveTo(player2:FindFirstChild("Head").Position)	
 				end
 			end
-		else
-			if player1 and player2 then
-				if player1:FindFirstChild("Humanoid") and player2:FindFirstChild("Humanoid") then
-					if player1.Humanoid.Health ~= 0 and player2.Humanoid.Health ~= 0 then
-						player1:MoveTo(player2.Head.Position)
-					end
-				end
-			end
+			return
+		end
+		
+		if gear_essentials.character_alive(player1) then
+			player1:MoveTo(player2.Head.Position)
 		end
 	end,
 }
